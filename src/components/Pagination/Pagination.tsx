42feb86e00cc7df64cac21efb820e1adfba8fbe5
nikizak34 <nikita2000.16.11@gmail.com>
2023-12-11 16:52:19 +0300

    add:classNames object

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -39,30 +39,32 @@ export function Pagination({
     onChange(pageNumber);
   };
 
-  const buttonLeft = clsx(Styles.page, disable.left && Styles.disabled);
-  const buttonRight = clsx(Styles.page, disable.right && Styles.disabled);
-  const arrayL = clsx(Styles.icon, disable.left && Styles.iconDisabled);
-  const arrayR = clsx(Styles.icon, disable.right && Styles.iconDisabled);
+  const classNames = {
+    buttonLeft: clsx(Styles.page, disable.left && Styles.disabled),
+    buttonRight: clsx(Styles.page, disable.right && Styles.disabled),
+    arrayL: clsx(Styles.icon, disable.left && Styles.iconDisabled),
+    arrayR: clsx(Styles.icon, disable.right && Styles.iconDisabled),
+  };
 
   return (
     <div className={Styles.paginator}>
       <button
         type="button"
         disabled={disable.left}
-        className={buttonLeft}
+        className={classNames.buttonLeft}
         onClick={handleFirstPageClick}
         aria-label="arrow left"
       >
-        <ArrayDoubleL className={arrayL} />
+        <ArrayDoubleL className={classNames.arrayL} />
       </button>
       <button
-        className={buttonLeft}
+        className={classNames.buttonLeft}
         type="button"
         onClick={handlePrevPageClick}
         disabled={disable.left}
         aria-label="arrow left"
       >
-        <ArrayL className={arrayL} />
+        <ArrayL className={classNames.arrayL} />
       </button>
       {pages.map((el, index) => (
         <Page
@@ -73,22 +75,22 @@ export function Pagination({
         />
       ))}
       <button
-        className={buttonRight}
+        className={classNames.buttonRight}
         type="button"
         onClick={handleNextPageClick}
         disabled={disable.right}
         aria-label="arrow right"
       >
-        <ArrowR className={arrayR} />
+        <ArrowR className={classNames.arrayR} />
       </button>
       <button
         onClick={handleLastPageClick}
-        className={buttonRight}
+        className={classNames.buttonRight}
         type="button"
         disabled={disable.right}
         aria-label="arrow right"
       >
-        <ArrayDoubleR className={arrayR} />
+        <ArrayDoubleR className={classNames.arrayR} />
       </button>
     </div>
   );
